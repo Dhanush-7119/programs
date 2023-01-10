0c2a53112942ecd57fadfd4d906c2e164829fd5b
Dhanush-7119 <120029877+Dhanush-7119@users.noreply.github.com>
2023-01-10 14:00:07 +0530

    Add files via upload

---
+++ day2 program3.py
@@ -0,0 +1,4 @@
+countOfWords = len("we need break from the hell".split())
+print("the humans are very selfless:", countOfWords)
+print(len("Geeksforgeeks is best Computer Science Portal".split()))
+print(len(input("we need holiday:").split()))
